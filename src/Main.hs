1ce57de2e41c5804bf842ee6f84a493ef13b9ede
Cody Goodman <codygman.consulting@gmail.com>
2016-08-20 23:36:44 -0500

    working joinOnId

--- src/Main.hs
+++ src/Main.hs
@@ -3,84 +3,110 @@
 module Main where
 
 import Data.Vinyl
-import Data.Vinyl.Functor
-import Control.Applicative
 import Control.Lens hiding (Identity)
-import Control.Lens.TH
-import Data.Char
-import Test.DocTest
 import Data.Singletons.TH
 import Data.Maybe
-import Data.List
 
 data Fields = Id | Name | Age | ActivityName deriving Show
 
-type Person = [Id, Name, Age]
-type Activity = [Id, ActivityName]
+type Person = ['Id, 'Name, 'Age]
+type Activity = ['Id, 'ActivityName]
 
 type family ElF (f :: Fields) :: * where
-  ElF Id = Int
-  ElF Name = String
-  ElF Age = Int
-  ElF ActivityName = String
+  ElF 'Id = Int
+  ElF 'Name = String
+  ElF 'Age = Int
+  ElF 'ActivityName = String
 
 newtype Attr f = Attr { _unAttr :: ElF f }
 makeLenses ''Attr
 genSingletons [ ''Fields ]
-instance Show (Attr Id) where show (Attr x) = "id: " ++ show x
-instance Show (Attr Name) where show (Attr x) = "name: " ++ show x
-instance Show (Attr Age) where show (Attr x) = "age: " ++ show x
-instance Show (Attr ActivityName) where show (Attr x) = "activity: " ++ x
+instance Show (Attr 'Id) where show (Attr x) = "id: " ++ show x
+instance Show (Attr 'Name) where show (Attr x) = "name: " ++ show x
+instance Show (Attr 'Age) where show (Attr x) = "age: " ++ show x
+instance Show (Attr 'ActivityName) where show (Attr x) = "activity: " ++ x
 
 (=::) :: sing f -> ElF f -> Attr f
 _ =:: x = Attr x
 
+joy :: Rec Attr ['Id, 'Name, 'Age]
 joy = (SId =:: 1)
    :& (SName =:: "Joy")
    :& (SAge =:: 28)
    :& RNil
-
+jon :: Rec Attr ['Id, 'Name, 'Age]
 jon = (SId =:: 0)
    :& (SName =:: "Jon")
    :& (SAge =:: 23)
    :& RNil
 
+karen :: Rec Attr ['Id, 'Name, 'Age]
 karen = (SId =:: 2)
    :& (SName =:: "Karen")
    :& (SAge =:: 15)
    :& RNil
 
-
+jonFootball :: Rec Attr ['Id, 'ActivityName]
 jonFootball = (SId =:: 0)
            :& (SActivityName =:: "football")
            :& RNil
 
+jonDancing :: Rec Attr ['Id, 'ActivityName]
 jonDancing = (SId =:: 0)
            :& (SActivityName =:: "dancing")
            :& RNil
 
-joyRacing = (SId =:: 0)
+joyRacing :: Rec Attr ['Id, 'ActivityName]
+joyRacing = (SId =:: 1)
            :& (SActivityName =:: "racing")
            :& RNil
 
-
+peopleRows :: [Rec Attr ['Id, 'Name, 'Age]]
 peopleRows = [joy, jon, karen]
+
+activitieRows :: [Rec Attr ['Id, 'ActivityName]]
 activitieRows = [jonFootball, jonDancing, joyRacing]
 
-printActvy :: (ActivityName ∈ fields) => Rec Attr fields -> IO ()
+printActvy :: ('ActivityName ∈ fields) => Rec Attr fields -> IO ()
 printActvy r = print (r ^. rlens SActivityName)
 
-isInPplIdx :: (Id ∈ fields) => [Int] -> Rec Attr fields -> Bool
-isInPplIdx peopleIdx actvyRow =  any (== True) . map (== 1) $ peopleIdx
+isInPplIdx :: ('Id ∈ fields) => [Int] -> Rec Attr fields -> Bool
+isInPplIdx peopleIdx actvyRow =  any (== True) . map (== actvyIdInt) $ peopleIdx
   where actvyIdInt = actvyRow ^. rlens SId . unAttr
 
--- joinOnId :: (Id ∈ fields) => [Rec Attr fields] -> [Rec Attr fields] -> Rec Attr fields
-joinOnId :: (Id ∈ fields) => [Rec Attr fields] -> [Rec Attr fields] -> IO ()
+mkJoinedRow :: [Rec Attr ['Id, 'ActivityName]] -> Rec Attr ['Id, 'Name, 'Age] ->  Rec Attr ['Id, 'Name, 'Age, 'ActivityName]
+-- mkJoinedRow :: [Rec Attr ['Id, 'ActivityName]] -> Rec Attr ['Id, 'Name, 'Age] -> [Rec Attr ['Id, 'ActivityName]]
+mkJoinedRow activities person = do
+  -- TODO map over people, matching actvy from filtered actvys for extra info and constructing new Rec
+  -- (SId =:: 0) :& (SActivityName =:: "dancing") :& RNil
+  -- filterMap (\r -> r ^. rlens SId . unAttr == person ^. rlens SId .unAttr)
+
+  let name = person ^. rlens SName . unAttr
+      age = person ^. rlens SAge . unAttr
+
+  case listToMaybe $ filter (\r -> r ^. rlens SId . unAttr == person ^. rlens SId . unAttr) activities of
+    (Just matchingActivity) -> do
+      let activityId = matchingActivity ^. rlens SId . unAttr
+          activityName = matchingActivity ^. rlens SActivityName . unAttr
+      let x = (SId =:: activityId) :& (SName =:: name) :& (SAge =:: age) :& (SActivityName =:: activityName) :& RNil
+      (x :: Rec Attr ['Id, 'Name, 'Age, 'ActivityName])
+    Nothing -> (SId =:: (negate 1)) :& (SName =:: name) :& (SAge =:: age) :& (SActivityName =:: "null") :& RNil
+
+
+joinOnId :: [Rec Attr ['Id, 'Name, 'Age]] -> [Rec Attr ['Id, 'ActivityName]] -> [Rec Attr ['Id, 'Name, 'Age, 'ActivityName]]
 joinOnId people activities = do
   let peopleIdx =(\r -> r ^. rlens SId . unAttr) <$> people
-      filteredActivites = filter (isInPplIdx peopleIdx) activities
-  print filteredActivites 
+  let filteredActivites = filter (isInPplIdx peopleIdx) activities
+  
+  -- TODO why does this give an error? mkJoinedRow returns a result that contains an 'Id
+  map (\p -> mkJoinedRow filteredActivites p) people
 
--- main :: IO ()
--- main = do
---   putStrLn "hello world"
+  -- undefined
+
+  -- filteredActivites
+  -- filteredActivites
+  -- print (filteredActivites)
+
+main :: IO ()
+main = do
+  putStrLn "hello world"
